954de7ca596cba38aca19059beae9dd742ffdfbe
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-10-30 16:19:43 +0700

    Menambahkan HorizontalDivider setelah form input

--- app/src/main/java/com/example/input_pengguna/TeksEdit.kt
+++ app/src/main/java/com/example/input_pengguna/TeksEdit.kt
@@ -79,5 +79,15 @@ fun FormDataDiri(modifier: Modifier
             }
 
         )
+        HorizontalDivider(
+            modifier = Modifier.padding(
+                bottom = dimensionResource(id = R.dimen.padding_medium),
+                top = dimensionResource(
+                    id = R.dimen.padding_medium
+                )),
+            thickness = dimensionResource(id = R.dimen.divider_tipis),
+            color = Color.DarkGray
+        )
+
     }
 }
